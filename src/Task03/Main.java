7a971b67cdcc2ded671637b5c4a03135758cee7b
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2023-12-02 23:56:06 +0200

    add task03 solution

---
+++ src/Task03/Main.java
@@ -0,0 +1,56 @@
+package Task03;
+
+import java.text.DecimalFormat;
+
+public class Main {
+
+    static double[] productCost;
+    static double totalCost;
+    static double averageCost;
+    final static String CURRENCY = "USD";
+
+
+    public static void main(String[] args) {
+
+        productCost = new double[]{14.75, 23.15, 32.12, 54.01, 11.23, 76.23, 43.28, 22.11, 62.70, 93.45};
+
+        totalCost = getTotalCost(productCost);
+        averageCost = getAverageCost(productCost);
+        getOutput();
+    }
+
+    public static double getTotalCost(double... prices){
+
+        double sum = 0;
+
+        for (double price:prices) {
+            sum +=price;
+        }
+
+        return sum;
+    }
+
+    public static double getAverageCost(double... prices){
+
+        double sum = 0;
+
+        for (double price:prices) {
+            sum += price;
+        }
+
+        return sum / prices.length;
+    }
+
+    public static void getOutput() {
+
+        String roundTotalCost = roundValue(totalCost);
+        String roundAverageCost = roundValue(averageCost);
+
+        System.out.println("Total cost of deliveries: " + CURRENCY + " " + roundTotalCost +
+                "\nAverage cost of delivery: " + CURRENCY + " " + roundAverageCost);
+    }
+
+    public static String roundValue(double value) {
+        return new DecimalFormat("#.00").format(value);
+    }
+}
